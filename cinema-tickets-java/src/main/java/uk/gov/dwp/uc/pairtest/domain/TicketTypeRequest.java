a5b3cf28a7c898bba0c024eb7ebef79f78f4a4a9
sabafshin <sabafshin@users.noreply.github.com>
2023-10-11 00:50:12 +0000

    setup repo for java sub project

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -6,7 +6,12 @@ package uk.gov.dwp.uc.pairtest.domain;
 
 public class TicketTypeRequest {
 
+    public enum Type {
+        ADULT, CHILD, INFANT
+    }
+
     private int noOfTickets;
+
     private Type type;
 
     public TicketTypeRequest(Type type, int noOfTickets) {
@@ -20,10 +25,6 @@ public class TicketTypeRequest {
 
     public Type getTicketType() {
         return type;
-    }
-
-    public enum Type {
-        ADULT, CHILD , INFANT
     }
 
 }
